d67e2e921ae86c25db9cfc4d28cbf5b22ab623ec
taeheon95 <dalo952003@gmail.com>
2021-06-27 16:40:41 +0900

    modify controller

--- src/main/java/com/gotcha/www/card/controller/CardController.java
+++ src/main/java/com/gotcha/www/card/controller/CardController.java
@@ -12,5 +12,4 @@ public class CardController {
         this.cardService = cardService;
     }
 
-
 }
--- src/main/java/com/gotcha/www/workList/controller/WorkListController.java
+++ src/main/java/com/gotcha/www/workList/controller/WorkListController.java
@@ -4,11 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gotcha.www.workList.dao.WorkListDAO;
 import com.gotcha.www.workList.service.WorkListService;
@@ -18,27 +17,22 @@ import com.gotcha.www.workList.vo.ListVO;
 
 @RestController
 public class WorkListController {
-	
+
+	private final WorkListService workListService;
+	private final Log log = LogFactory.getLog(this.getClass());
+
 	@Autowired
-	WorkListService wokrListService;
-	
-	@Autowired
-	WorkListDAO workListDAO;
+	public WorkListController(WorkListService workListService){
+		this.workListService = workListService;
+	}
 	
 	//워크리스트에 리스트 뿌려줌
-	@RequestMapping("/main/wsList/list") 
+	@RequestMapping("/main/wsList/list")
 	public @ResponseBody List<ListVO> selectList( @RequestBody HashMap<String,String> map)throws Exception{
 		String listWsid = map.get("ws_id");
-		System.out.println("listWsid: "+listWsid);
-		List<ListVO> listList = wokrListService.selectList(listWsid);
-		//리스트와 카드를 한 번에 가져오고 싶으면 아래 주석 해제
-//		for(ListVO list : listList) {
-//		List<CardVO> cardList = wokrListService.selectCartList(list.getList_id());
-//		list.setCard(cardList);
-//		}
-		System.out.println("listList: "+listList);
-		//System.out.println(listVO.getWs_id());
-		
+		log.info("\n"+"list work space id: "+listWsid);
+		List<ListVO> listList = workListService.selectList(listWsid);
+		listList.forEach(log::info);
 		return listList;
 	}
 	
@@ -46,7 +40,7 @@ public class WorkListController {
 //	@RequestMapping("/main/wsList/list/card")
 //	public @ResponseBody List<Map<String, CardVO>> selectList(@RequestBody CardVO cardVO)throws Exception{
 //		System.out.println("list_id: "+cardVO.getList_id());
-//		List<Map<String, CardVO>> cardList = wokrListService.selectCard(cardVO);
+//		List<Map<String, CardVO>> cardList = workListService.selectCard(cardVO);
 //		System.out.println("cardList: "+cardList);
 //		return cardList;
 //	}
@@ -55,7 +49,7 @@ public class WorkListController {
 		public @ResponseBody List<CardVO> selectCard(@RequestBody HashMap<String,String> map)throws Exception{
 			String cardWsid = map.get("ws_id");
 			System.out.println("cardWsid: "+cardWsid);
-			List<CardVO> cardList = wokrListService.selectCard(cardWsid);
+			List<CardVO> cardList = workListService.selectCard(cardWsid);
 			System.out.println("cardList: "+cardList);
 			return cardList;
 		}
