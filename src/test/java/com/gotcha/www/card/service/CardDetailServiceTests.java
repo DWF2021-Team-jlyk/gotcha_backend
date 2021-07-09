6142b8548d23e55d82ef098c4102d86d5282b9cb
taeheon95 <dalo952003@gmail.com>
2021-07-09 09:06:40 +0900

    병합전 커밋

--- src/test/java/com/gotcha/www/card/service/CardDetailServiceTests.java
+++ src/test/java/com/gotcha/www/card/service/CardDetailServiceTests.java
@@ -14,17 +14,17 @@ import java.util.Optional;
 public class CardDetailServiceTests {
 
     private Log log = LogFactory.getLog(this.getClass());
-
-    @Autowired
-    private CardDetailService service;
-
-    @Test
-    public void getCardInfoTest(){
-        CardDetailVO cardDetail = service.getCardInfo(1);
-//        log.info("\n"+cardDetail.getCardDTO());
-        log.info("\n"+cardDetail.getCardActs());
-        log.info("\n"+cardDetail.getCardFiles());
-        log.info("\n"+cardDetail.getCardTodos());
-        log.info("\n"+cardDetail.getCardMembers());
-    }
+//
+//    @Autowired
+//    private CardDetailService service;
+//
+//    @Test
+//    public void getCardInfoTest(){
+//        CardDetailVO cardDetail = service.getCardInfo(1);
+////        log.info("\n"+cardDetail.getCardDTO());
+//        log.info("\n"+cardDetail.getCardActs());
+//        log.info("\n"+cardDetail.getCardFiles());
+//        log.info("\n"+cardDetail.getCardTodos());
+//        log.info("\n"+cardDetail.getCardMembers());
+//    }
 }
