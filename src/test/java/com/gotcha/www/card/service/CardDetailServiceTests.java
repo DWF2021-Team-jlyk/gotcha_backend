b2b7c267604a45ec5b62c7ccb7b23980480bf530
taeheon95 <dalo952003@gmail.com>
2021-07-13 15:25:42 +0900

    Merge branch 'boyoung' into taeheon
    # Conflicts:
    #	src/main/resources/mappers/WorkListMapper/WorkListMapper.xml

--- src/test/java/com/gotcha/www/card/service/CardDetailServiceTests.java
+++ src/test/java/com/gotcha/www/card/service/CardDetailServiceTests.java
@@ -1,6 +1,6 @@
 package com.gotcha.www.card.service;
 
-import com.gotcha.www.card.vo.CardDetailVO;
+//import com.gotcha.www.card.vo.CardDetailVO;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.Assertions;
